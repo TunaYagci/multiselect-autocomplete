b20f84d5563103e3d27b5644ce4f3593e4df046b
Tuna Yagci <tuna.yagci@atarlabs.io>
2019-11-07 14:33:17 +0300

    updated App.vue for repo link

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/docs">Docs</router-link>
+    <div id="app">
+        <a style="position:absolute;top:5px;right:5px;margin:0;padding:5px 3px;"
+           href="https://github.com/TunaYagci/multiselect-autocomplete"><h3>Repository</h3></a>
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+            |
+            <router-link to="/docs">Docs</router-link>
+        </div>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
-  #app {
+    #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
