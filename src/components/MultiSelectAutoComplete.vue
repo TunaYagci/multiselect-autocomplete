aeab80d74b8193b6972e13cb4bb9ba493a5ef291
Tuna Yagci <tuna.yagci@atarlabs.io>
2019-11-07 12:33:51 +0300

    added bootstrap as dependency

--- src/components/MultiSelectAutoComplete.vue
+++ src/components/MultiSelectAutoComplete.vue
@@ -59,6 +59,7 @@
 
 <script>
     import differenceWith from 'lodash.differencewith';
+    import 'bootstrap/dist/css/bootstrap.min.css'
 
     export default {
         name: "multi-select-auto-complete",
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,6 +22,7 @@
 
 <script>
     import StarWarsAutoComplete from "../components/StarWarsAutoComplete";
+    import 'bootstrap/dist/css/bootstrap.min.css'
 
     export default {
         name: 'home',
